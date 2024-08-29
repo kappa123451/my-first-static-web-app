8fdb564e791f316bd8d6bf2a472779a0a54a0017
kappa123451 <kasper900322@gmail.com>
2024-08-29 20:32:29 +1000

    background

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App, { HeadLine } from './Component/App'; // Import both App and HeadLine
+import RootLayout from 'layout';
 
 ReactDOM.render(
+  <RootLayout>
   <div>
     <HeadLine />
     <App />
   </div>,
   document.getElementById('root')
+  </RootLayout>
 );
