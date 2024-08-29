4397713a4177f61762ba910749fc74b98404b3d4
kappa123451 <kasper900322@gmail.com>
2024-08-29 20:46:39 +1000

    fix layout try 2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import RootLayout from './Layout';
-import App, { HeadLine } from './Component/App'; // Import both App and HeadLine
-
+import RootLayout from './Layout'; // Ensure casing matches the actual file name
+import App, { HeadLine } from './Component/App';
 
 ReactDOM.render(
   <RootLayout>
-  <div>
     <HeadLine />
     <App />
-  </div>,
   </RootLayout>,
   document.getElementById('root')
 );
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,18 +1,19 @@
+import React from 'react';
 
 
-
-export default function RootLayout() {
-    return (
-      <html
-        lang="en"
-        className={cx(
-          'text-black bg-white dark:text-white dark:bg-black',
-        )}
-      >
-        <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          </main>
-        </body>
-      </html>
-    )
-  }
+export default function RootLayout({ children }) {
+  return (
+    <div
+      lang="en"
+      className={cx(
+        'text-black bg-white dark:text-white dark:bg-black',
+      )}
+    >
+      <div className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
